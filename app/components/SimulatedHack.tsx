c14476ca913a162b35125e9f0809dfec4960f0dd
Dicksen Tan <tandicksen@gmail.com>
2025-06-20 09:31:12 +0700

    Create SimulatedHack.tsx
    component to simulate hack based on vuln description in cli style log

---
+++ app/components/SimulatedHack.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useState } from "react"
+
+export default function SimulatedHack({desc} : {desc: string}){
+    const [hack, setHack] = useState<string>('')
+    const handleHacking = async() =>{
+        const response = await fetch('/api/hack',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({desc})
+        })
+        const reader = response.body?.getReader()
+        const decoder = new TextDecoder('utf-8')
+        if(!reader) return
+        while(true){
+            const {value, done} = await reader.read()
+            if(done) break
+            const chunk = decoder.decode(value)
+            setHack(prev => prev + chunk)
+        }
+    }
+    return(
+        <div className="pt-4">
+            <button
+                onClick={handleHacking}
+                className="bg-red-500 text-white px-4 py-2 rounded-xl hover:bg-red-600 transition cursor-pointer mb-2"
+            >
+                🚨 SIMULATE HACK & HOW TO PREVENT IT 🛡️
+            </button>
+            {hack && 
+            <pre className="bg-black scrollbar-thin text-green-400 font-mono p-4 rounded-md overflow-x-auto w-full text-sm leading-relaxed">
+                {hack}
+                </pre>}
+            </div>
+    )
+}
