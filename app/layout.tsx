1ff0181d035e24e5759c51026d7f2776e7baeb4b
Dicksen Tan <tandicksen@gmail.com>
2025-06-20 09:22:10 +0700

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,19 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Shift-Left Security",
   description: "Scan Vulnerabilities",
+   keywords: [
+    'DevSecOps', 
+    'ShiftLeft Security',
+    'Snyk Alternative',
+    'Trivy',
+    'Semgrep',
+    'OWASP Top 10',
+    'SAST/DAST',
+    'CI/CD Security',
+    'Scan Vulnerability',
+    'Scan repo',
+    'Scan GitHub'
+  ],
 };
 
 export default function RootLayout({
